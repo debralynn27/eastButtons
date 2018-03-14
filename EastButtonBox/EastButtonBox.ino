eb9a7f3626703da4689a86486e4de63e7005ae72
Jeff Wilkinson <jwilkinson@nvidia.com>
2018-03-14 12:26:44 -0500

    Updated for SXSW install
    Meow Wolf + FTBD install at Empire Control Room for SXSW
    (minor changes, mostly)
    * replaced buttonPin constants with defines.
    * removed some unused globals.
    * started to replace "last button pressed" global with passed function arguments and return values, but realized why it had to be global.
    * adjusted bpm() constants, to optimize "placid" mood on current strand & segment lengths

--- EastButtonBox/EastButtonBox.ino
+++ EastButtonBox/EastButtonBox.ino
@@ -1,125 +1,141 @@
-//Three buttons doing some things. Debra Lemak 10/14/17
-//Update 02/13/18
+// Three buttons doing some things. 
+// Debra Lemak 10/14/17
+// Update      02/13/18
+// Wilkinson   03/09/18
 
 #include <FastLED.h>
-#define NUM_LEDS1 40
-#define NUM_LEDS2 120
-#define NUM_LEDS3 116
-#define DATA_PIN1 9
-#define DATA_PIN2 10
-#define DATA_PIN3 11
+
+#define NUM_LEDS1  75
+#define NUM_LEDS2 118
+#define NUM_LEDS3 144
+
+#define DATA_PIN1   9
+#define DATA_PIN2  10
+#define DATA_PIN3  11
+
+#define BUTTON_PIN1 4
+#define BUTTON_PIN2 3
+#define BUTTON_PIN3 6
+#define BUTTON_NONE 0
 
 #define BRIGHTNESS  255
 #define FRAMES_PER_SECOND 60
 
+// LED color arrays
 CRGB leds1[NUM_LEDS1];
 CRGB leds2[NUM_LEDS2];
 CRGB leds3[NUM_LEDS3];
-CRGBPalette16 gPal;
 
+// color selection globals
+CRGBPalette16 gPal;  // setting here causes hang?  wtf?
+uint8_t       gHue = 00; // rotating "base color" 
 
-// constants won't change. They're used here to set pin numbers and fade:
-const int buttonPin01 = 4;     // the number of the pushbutton pin
-const int buttonPin02 = 3;     // the number of the pushbutton pin
-const int buttonPin03 = 6;     // the number of the pushbutton pin
+// replace global with return value
+// int buttonPressed = 0;
 
-//bool gReverseDirection = false;
-
-// variables will change:
-int buttonPressed = 0;
-
-
-int ledMode = 0;
-uint8_t gHue = 0; // rotating "base color" used by many of the patterns
-long boredomTimer = 0; 
+//CRGBPalette16 currentPalette(ForestColors_p);
+//CRGBPalette16 targetPalette(OceanColors_p);
 
 
 ///////Pulling in new color sequence from Andrew Tuline, Title: inoise8_pal_demo.ino
-static uint16_t dist;         // A random number for our noise generator.
-uint16_t scale = 30;          // Wouldn't recommend changing this on the fly, or the animation will be really blocky.
+
+//uint16_t scale = 30;          // Wouldn't recommend changing this on the fly, or the animation will be really blocky.
 uint8_t maxChanges = 48;      // Value for blending between palettes.
  
-CRGBPalette16 currentPalette(ForestColors_p);
-CRGBPalette16 targetPalette(OceanColors_p);
+
 /////////////////////////////////////////////////////////////////
 
 void setup() {
-  Serial.begin(9600);
+  // set up palette global for color effects  
+  gPal = CRGBPalette16(CRGB::Green, CRGB::Blue); 
 
-  pinMode(buttonPin01, INPUT);
-  pinMode(buttonPin02, INPUT);
-  pinMode(buttonPin03, INPUT);
+  // set up the Buttons
+  Serial.begin(9600);
+  pinMode(BUTTON_PIN1, INPUT);
+  pinMode(BUTTON_PIN2, INPUT);
+  pinMode(BUTTON_PIN3, INPUT);
+
+  // set up the LEDs
+  FastLED.setBrightness(BRIGHTNESS); 
   FastLED.addLeds<WS2811, DATA_PIN1, RGB>(leds1, NUM_LEDS1);
   FastLED.addLeds<WS2811, DATA_PIN2, RGB>(leds2, NUM_LEDS2);
   FastLED.addLeds<WS2811, DATA_PIN3, RGB>(leds3, NUM_LEDS3);
-  FastLED.setBrightness( BRIGHTNESS );
+  
+  // clear and fill with something
   FastLED.clear();
-  gPal = CRGBPalette16( CRGB::Green,  CRGB::Blue);
-//////////from Tuline
-  dist = random16(12345);          // A semi-random number for our noise generator
-/////////
+  renderEffects(BUTTON_PIN1); // pretend a button was pushed
+  FastLED.show();
 }
 
+int gLastButton;
 /*
  * Main loop
  */
 void loop() {
-  checkInputs();
-  renderEffects();
+  int buttonPressed = checkInputs();
+  
+  if (buttonPressed != BUTTON_NONE) {
+    gLastButton = buttonPressed;   
+  }
+  renderEffects(gLastButton);
   FastLED.show();
+
+
+  
   //FastLED.delay(1000/FRAMES_PER_SECOND); 
 }
 
 /**
  * Check our inputs and set the button state
  */
-void checkInputs() {
-  
- if (digitalRead(buttonPin01) == HIGH) {
-    buttonPressed = buttonPin01;
-    return buttonPressed;
+int checkInputs() {
+ int buttonPressed = BUTTON_NONE;
+ 
+ if (digitalRead(BUTTON_PIN1) == HIGH) {
+    // highest priority button
+    buttonPressed = BUTTON_PIN1;
   } 
-  
-  if (digitalRead(buttonPin02) == HIGH) {
-    buttonPressed = buttonPin02;
-    return buttonPressed;
+  else if (digitalRead(BUTTON_PIN2) == HIGH) {
+    buttonPressed = BUTTON_PIN2;
   } 
-  
-  if (digitalRead(buttonPin03) == HIGH) {
-    buttonPressed = buttonPin03;
-    return buttonPressed;
+  else if (digitalRead(BUTTON_PIN3) == HIGH) {
+    // lowest priority button
+    buttonPressed = BUTTON_PIN3;
   } 
   else {
-  buttonPressed = 0;
-  Serial.println(buttonPressed);
+    // bail out early
+    return BUTTON_NONE;
   }
+
+  Serial.println(buttonPressed);
+  return buttonPressed;
 }
 
 
 /**
  * Handle the button state to render effects
  */
-void renderEffects() {
+void renderEffects(int buttonPressed) {
     switch (buttonPressed) {
-    case buttonPin01:
+    case BUTTON_PIN1:
      Serial.println("Button 1 pressed.");  
       bpm(leds1, NUM_LEDS1);
       bpm(leds2, NUM_LEDS2);
       bpm(leds3, NUM_LEDS3);
       break;
-    case buttonPin02:
+    case BUTTON_PIN2:
       Serial.println("Button 2 pressed.");  
       juggle(leds1, NUM_LEDS1);
       juggle(leds2, NUM_LEDS2);
       juggle(leds3, NUM_LEDS3); 
       break;
-    case buttonPin03:
+    case BUTTON_PIN3:
       Serial.println("Button 3 pressed."); 
       rainbowWithGlitter(leds1, NUM_LEDS1); 
       rainbowWithGlitter(leds2, NUM_LEDS2);
       rainbowWithGlitter(leds3, NUM_LEDS3); 
       break;
-    case 0:
+    case BUTTON_NONE:
       Serial.println("No button pressed.");  
       break;
   }
@@ -144,11 +160,11 @@ void juggle(CRGB* strand, int numlights) {
 void bpm(CRGB* strand, int numlights)
 {
   // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
-  uint8_t BeatsPerMinute = 62;
+  uint8_t BeatsPerMinute = 8;
   CRGBPalette16 palette = gPal;
   uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
   for( int i = 0; i < numlights; i++) { 
-    strand[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
+    strand[i] = ColorFromPalette(palette, gHue+(i*2.60), beat-gHue+(i*3.33));
   }
 }
 
